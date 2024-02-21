063dc8200eceb9e677929da598764571369be484
Hazesz <ihenriquekogavigato@gmail.com>
2024-02-21 15:40:03 -0300

    Esqueleto do projeto iniciado

---
+++ Trabalho.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+    struct cadastro //informações do paciente
+    {
+    char Npet[15];
+    char Ndono[25];
+    char Espc[30];
+    int idade;
+    char sintoma[50];
+    char diagnostico[150];
+    };
+    
+    int consulta(char Npet){
+        //ponteiro direcionado para struct
+        //consultar informações no file
+    }
+
+    int alterar_reg(char Npet){
+        //ponteiro direcionado a struct
+        //alterar informações no file
+    }
